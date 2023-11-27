6eb56430e3da65e8b9e56975b655a055fe2f567a
evilbutchi <151439579+evilbutchi@users.noreply.github.com>
2023-11-27 11:03:51 +0800

    Updated Bloodbank_Dashboard.js
    gi navigate na nako ang available donors ug para next page kadtng hospital information pero mag update pako balik ani kay mag create pako sa code sa hospital information

--- src/Bloodbank_Dashboard.js
+++ src/Bloodbank_Dashboard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Button, TextField } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import heroLogo from './Image Hero.svg';
@@ -6,11 +6,17 @@ import circleWithBlood from './circlewithblood.png';
 import Image5 from  './Image5.png';
 import rectangle11 from './rectangle11.png';
 import rectangle12 from './rectangle12.png';
+import Vector from './Vector.png';
 
 const BloodbankDashboard = () => {
   const navigate = useNavigate();
 
   const Header = () => {
+    const [query, setQuery] = useState('');
+  
+    const handleSearchChange = (event) => {
+      setQuery(event.target.value);
+    };
     return (
       <div className="Header" style={{ position: 'relative' }}>
         <div className="LogoAndTitle" style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
@@ -25,22 +31,43 @@ const BloodbankDashboard = () => {
           
           {/* TextField with increased marginRight */}
           <TextField
-            id="outlined-basic"
-            label="Search for Blood Banks"
-            variant="outlined"
-            style={{ width: 288, height: 55, background: 'white', borderRadius: 100, border: '1px #FFC3C3 solid', marginLeft: -160, marginRight: 20, marginTop: 100 }}
-          />
+          id="outlined-basic"
+          label="Search for Blood Banks"
+          variant="outlined"
+          style={{ marginLeft: -160, marginRight: 20, marginTop: 100, border: '1px #FFC3C3 solid', borderRadius: 100, }}
+          InputProps={{
+            endAdornment: (
+              <img
+                src={Vector}
+                alt="Search Icon"
+                style={{ width: 17, height: 18, marginLeft: 8, }}
+              />
+            ),
+            style: {
+              width: 325,
+              height: 55,
+              display: 'flex', // Align icon at the end
+              justifyContent: 'flex-end', // Align icon at the end
+            },
+          }}
+          onChange={handleSearchChange}
+        />
+
+
         </div>
 
         {/* Navigation with increased marginRight and aligned to circle-with-blood */}
         <div className="Navigation" style={{ position: 'absolute', left: '50%', transform: 'translateX(-50%)', top: '20%', display: 'flex', alignItems: 'center', justifyContent: 'center', marginTop: 0 }}>
         <div className="Home" style={{color: '#FF0000', fontSize: 18, fontFamily: 'Poppins', fontWeight: '500', textTransform: 'capitalize', letterSpacing: 0.90, wordWrap: 'break-word'}}>home</div>
-          <div className="AvailableDonors" style={{color: 'black', fontSize: 18, fontFamily: 'Poppins', fontWeight: '500', textTransform: 'capitalize', letterSpacing: 0.90, wordWrap: 'break-word'}}>Available Donors</div>
+          <div className="AvailableDonors" style={{color: 'black', fontSize: 18, fontFamily: 'Poppins', fontWeight: '500',
+           textTransform: 'capitalize', letterSpacing: 0.90, wordWrap: 'break-word'}}onClick={() => navigate("/available-donors")}>Available Donors</div>
         </div>
         
         {/* Buttons with increased marginRight */}
         <div className="Buttons" style={{ gap: '30px', marginRight: '50px', marginTop: '-65px' }}>
-          <Button variant="outlined" onClick={() => navigate("/Register_User")} style={{ color: '#861530', fontSize: 18, fontFamily: 'Poppins', fontWeight: '800', textTransform: 'capitalize', letterSpacing: 0.90, wordWrap: 'break-word', borderRadius: 5, border: '2px solid #861530' }} >Logout</Button>
+          <Button variant="outlined" onClick={() => navigate("/Register_User")} style={{ color: '#861530', fontSize: 18, fontFamily: 'Poppins', fontWeight: '800', textTransform: 'capitalize', letterSpacing: 0.90,
+           wordWrap: 'break-word', borderRadius: 5, border: '2px solid #861530',width: 150,
+           height: 55, }} >Logout</Button>
         </div>
       </div>
     );
@@ -75,12 +102,13 @@ const BloodbankDashboard = () => {
     <div className="App">
       {/* Header */}
       
+      
       <Header />
 
       {/* Content Section */}
       <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginLeft: 20, marginRight: 20, flexDirection: 'column' }}>
         <div style={{ textAlign: 'right', marginRight: 10 }}>
-          <div className="Hospitals" style={{width: 226, height: 48, color: '#861530', fontSize: 24, fontFamily: 'Poppins', fontWeight: '700', wordWrap: 'break-word', marginLeft: -790, marginRight: 20, marginTop: 20 }}>Hospitals</div>
+          <div className="Hospitals" style={{width: 226, height: 48, color: '#861530', fontSize: 24, fontFamily: 'Poppins', fontWeight: '700', wordWrap: 'break-word', marginLeft: -760, marginRight: 20, marginTop: 20 }}>Hospitals</div>
          
         </div>
 
@@ -90,9 +118,9 @@ const BloodbankDashboard = () => {
       </div>
 
       {/* Rest of the content */}
-      <div className="Content" style={{ marginTop: 20, marginLeft: -800, marginRight: 20 }}>
+      <div className="Content" style={{ marginTop: 20, marginLeft: -750, marginRight: 20 }}>
   {/* Cebu Doctors' University Hospital */}
-  <div style={{ position: 'relative', textAlign: 'center', marginTop: 20 }}>
+  <div style={{ position: 'relative', textAlign: 'center', marginTop: 20 }} onClick={() => navigate("/bloodbank_hospital-information")}>
     <img
       className="Image5"
       src={Image5}
